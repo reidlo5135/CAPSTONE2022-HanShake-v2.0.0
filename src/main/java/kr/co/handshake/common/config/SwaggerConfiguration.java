06b47e2a86aa4b09b6604b6263d21770e6bf6ebd
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-03 16:28:17 +0900

    REFACTOR: Move Directory BaseEntity.java, SwaggerConfiguration.java

--- src/main/java/kr/co/handshake/common/config/SwaggerConfiguration.java
+++ src/main/java/kr/co/handshake/common/config/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package kr.co.handshake.common;
+package kr.co.handshake.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +8,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SwaggerConfiguration {
