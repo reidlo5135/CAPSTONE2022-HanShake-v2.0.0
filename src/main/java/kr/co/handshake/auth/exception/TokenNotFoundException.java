098aa64b4b23f782c2214963b3c15ae9b7fc5e24
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-08 08:56:55 +0900

    REFACTOR: Delete Auth Classes

--- src/main/java/kr/co/handshake/auth/exception/TokenNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package kr.co.handshake.auth.exception;
-
-public class TokenNotFoundException extends RuntimeException{
-    private static final String MESSAGE = "토큰이 없습니다.";
-
-    public TokenNotFoundException() {
-        super(MESSAGE);
-    }
-}
