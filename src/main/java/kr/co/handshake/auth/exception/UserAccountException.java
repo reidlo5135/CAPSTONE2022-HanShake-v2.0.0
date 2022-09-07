2a5791f7c119b920bae4175e7bc69ac54b476bdc
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-08 08:57:13 +0900

    REFACTOR: Delete User Classes

--- src/main/java/kr/co/handshake/auth/exception/UserAccountException.java
+++
@@ -1,9 +0,0 @@
-package kr.co.handshake.auth.exception;
-
-public class UserAccountException extends RuntimeException{
-    private static final String MESSAGE = "접근할 수 없는 유저입니다.";
-
-    public UserAccountException() {
-        super(MESSAGE);
-    }
-}
--- src/main/java/kr/co/handshake/user/exception/UserCreateException.java
+++
@@ -1,9 +0,0 @@
-package kr.co.handshake.user.exception;
-
-public class UserCreateException extends RuntimeException{
-    private static final String MESSAGE = "입력값을 확인해주세요";
-
-    public UserCreateException(Exception e) {
-        super(MESSAGE);
-    }
-}
