7d522a2feb02210f384931f1c40e1ef5f3e3a3a9
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-09 11:14:22 +0900

    REFACTOR: Move Directory WebMvcConfiguration.java, JpaAuditingConfiguration.java

--- server/boot/src/main/java/kr/co/handshake/common/config/JpaAuditingConfiguration.java
+++ server/boot/src/main/java/kr/co/handshake/common/config/JpaAuditingConfiguration.java
@@ -1,4 +1,4 @@
-package kr.co.handshake.web;
+package kr.co.handshake.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
