1a00f623f1881a917009e50988e5cda69ad268c8
강준모 <201840103@email.daelim.ac.kr>
2022-09-10 17:08:09 +0900

    REFACTOR: Apply Eslint Prettier

--- server/express/src/app.ts
+++ server/express/src/app.ts
@@ -1,15 +1,16 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import loaders from "./loaders";
+
 dotenv.config();
 
 async function startServer() {
   const app: express.Application = express();
-  await loaders({expressApp:app});
+  await loaders({ expressApp: app });
   const port = process.env.PORT || 5000;
   app.listen(port, async () => {
     console.log(`Express app.ts -> Connected to http://localhost:${port}`);
   });
 }
 
-startServer();
+export = startServer();
--- server/express/src/services/diet/DietService.ts
+++ server/express/src/services/diet/DietService.ts
@@ -1,36 +1,45 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from "axios";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cheerio = require("cheerio");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const iconv = require("iconv-lite");
 
-const getHtml = async (url:String) => {
-    console.log('GetHTML URL : ', url);
-    try {
-        return await axios.get(url);
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-function crawlAll() : Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-        getHtml('https://www.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=1470')
-            .then(html => {
-                let list: any[] = [];
-                const $ = cheerio.load(html.data);
-                const $bodyList = $('table.lineTop_tb2').children('tbody');
-
-                $bodyList.each(function(i:any, elem:Element):any {
-                    list[i] = {
-                        corner: $(elem).find('tr').text(),
-                    };
-                });
-
-                const data = list.filter(n => n.corner);
-                console.log('DATA : ', data);
-                resolve(data);
-            });
+// eslint-disable-next-line consistent-return
+const getHtml = async (url: string) => {
+  console.log("GetHTML URL : ", url);
+  try {
+    return await axios({
+      url,
+      method: "GET",
+      responseType: "arraybuffer",
     });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+function crawlAll(): Promise<any> {
+  return new Promise<any>((resolve, reject) => {
+    getHtml("https://www.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=1470").then(
+      (html: any) => {
+        const list: any[] = [];
+        const $ = cheerio.load(iconv.decode(html.data, "UTF-8"));
+        const $bodyList = $("table.lineTop_tb2 tbody");
+        $bodyList.each(function (i: any, elem: Element): any {
+          console.log("find : ", $(elem).html());
+          list[i] = {
+            corner: $(elem).find("tr:nth-of-type(1) > th").text(),
+          };
+        });
+
+        const data = list.filter((n) => n.corner);
+        console.log("DATA : ", data);
+        resolve(data);
+      }
+    );
+  });
 }
 
 export = {
-    crawlAll
-}
+  crawlAll,
+};
