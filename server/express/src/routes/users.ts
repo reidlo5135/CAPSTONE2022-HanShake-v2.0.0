88332a0958b053842f65d614c7d8817ed836c59e
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-10 13:54:50 +0900

    REFACTOR: Delete unused router files

--- server/express/src/routes/users.ts
+++
@@ -1,8 +0,0 @@
-import express, {Request, Response, NextFunction} from "express";
-const router = express.Router();
-
-router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.send('respond with a resource');
-});
-
-export = router;
