e2d54598644fa12ad4a540704fb795b50e87c56e
강준모 <201840103@email.daelim.ac.kr>
2022-09-11 20:02:03 +0900

    REFACTOR: Apply EsLint Prettier

--- server/express/src/services/diet/DietService.ts
+++ server/express/src/services/diet/DietService.ts
@@ -1,26 +1,37 @@
+import { ElementHandle } from "puppeteer-core";
 import crawlService from "../crawling/CrawlService";
-import {ElementHandle} from "puppeteer-core";
 
 const URL = "https://www.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=1470";
-const SELECTOR = "#contents > article > div > div.menu_tb > div.lineTop_tbArea.tbScroll > table > tbody";
+const SELECTOR =
+  "#contents > article > div > div.menu_tb > div.lineTop_tbArea.tbScroll > table > tbody";
 
 function crawlDietAll(): Promise<any> {
+  // eslint-disable-next-line no-async-promise-executor
   return new Promise<any>(async (resolve, reject) => {
-    await crawlService.commonCrawl(URL, SELECTOR).then(
-        async (response:ElementHandle) => {
-          const table = await response.evaluate(
-              () => Array.from(
-                  document.querySelectorAll("#contents > article > div > div.menu_tb > div.lineTop_tbArea.tbScroll > table > tbody > tr"),
-                  row => Array.from(row.querySelectorAll('th, td'), cell => cell.textContent)
+    await crawlService
+      .commonCrawl(URL, SELECTOR)
+      .then(async (response: ElementHandle) => {
+        const table = await response.evaluate(() =>
+          Array.from(
+            document.querySelectorAll(
+              "#contents > article > div > div.menu_tb > div.lineTop_tbArea.tbScroll > table > tbody > tr"
+            ),
+            (row) =>
+              Array.from(
+                row.querySelectorAll("th, td"),
+                (cell) => cell.textContent
               )
-          );
-          console.log("Table : ", table);
-          resolve(table);
-        }
-    ).catch((err:Error) => {reject(err)});
+          )
+        );
+        console.log("Table : ", table);
+        resolve(table);
+      })
+      .catch((err: Error) => {
+        reject(err);
+      });
   });
 }
 
 export = {
-  crawlDietAll
+  crawlDietAll,
 };
