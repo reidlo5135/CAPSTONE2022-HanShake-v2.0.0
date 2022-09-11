33812416d1500310aa856d645772d72c9ff4f9cd
강준모 <201840103@email.daelim.ac.kr>
2022-09-11 20:06:26 +0900

    REFACTOR: Apply EsLint Prettier

--- server/express/src/api/diet/DietController.ts
+++ server/express/src/api/diet/DietController.ts
@@ -1,25 +1,25 @@
 import { Request, Response } from "express";
 import service from "../../services/diet/DietService";
 
-const findDietAll = async (req:Request, res: Response) => {
-    service.crawlDietAll().then(
-        (resolve: Response) => {
-            console.log(
-                "DietController findPuppe promise stringify : ",
-                JSON.stringify(resolve)
-            );
-            res.status(200).send({ code: 0, message: "success", data: resolve });
-        },
-        (reject: Error) => {
-            console.log(
-                "DietController findPuppe promise reject : ",
-                JSON.stringify(reject)
-            );
-            res.send({ code: -1, message: "failed", error: reject });
-        }
-    )
-}
+const findDietAll = async (req: Request, res: Response) => {
+  service.crawlDietAll().then(
+    (resolve: Response) => {
+      console.log(
+        "DietController findPuppe promise stringify : ",
+        JSON.stringify(resolve)
+      );
+      res.status(200).send({ code: 0, message: "success", data: resolve });
+    },
+    (reject: Error) => {
+      console.log(
+        "DietController findPuppe promise reject : ",
+        JSON.stringify(reject)
+      );
+      res.send({ code: -1, message: "failed", error: reject });
+    }
+  );
+};
 
 export = {
-  findDietAll
+  findDietAll,
 };
--- server/express/src/services/crawling/CrawlService.ts
+++ server/express/src/services/crawling/CrawlService.ts
@@ -1,21 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const puppeteer = require("puppeteer-core");
 
-async function commonCrawl(url:string, selector:string) {
-    console.log("CrawlService commonCrawl url : ", url, ", selector : ", selector);
-    const browser = await puppeteer.launch({
-        product: "chrome",
-        executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-        headless: true,
-        defaultViewport: {
-            width:1200,
-            height:900
-        }
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
-    return await page.$(selector);
+async function commonCrawl(url: string, selector: string) {
+  console.log(
+    "CrawlService commonCrawl url : ",
+    url,
+    ", selector : ",
+    selector
+  );
+  const browser = await puppeteer.launch({
+    product: "chrome",
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    headless: true,
+    defaultViewport: {
+      width: 1200,
+      height: 900,
+    },
+  });
+  const page = await browser.newPage();
+  await page.goto(url);
+  return await page.$(selector);
 }
 
 export = {
-    commonCrawl
-}
+  commonCrawl,
+};
