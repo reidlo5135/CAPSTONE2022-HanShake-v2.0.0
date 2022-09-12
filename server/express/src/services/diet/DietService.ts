9b72faf898ac1cdb8c90259f8e9b8c70e1b17b6f
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-12 11:07:57 +0900

    REFACTOR: Modify Diet Crawling logic

--- server/express/src/services/diet/DietService.ts
+++ server/express/src/services/diet/DietService.ts
@@ -8,27 +8,52 @@ const SELECTOR =
 function crawlDietAll(): Promise<any> {
   // eslint-disable-next-line no-async-promise-executor
   return new Promise<any>(async (resolve, reject) => {
-    await crawlService
-      .commonCrawl(URL, SELECTOR)
-      .then(async (response: ElementHandle) => {
-        const table = await response.evaluate(() =>
-          Array.from(
-            document.querySelectorAll(
-              "#contents > article > div > div.menu_tb > div.lineTop_tbArea.tbScroll > table > tbody > tr"
-            ),
-            (row) =>
-              Array.from(
-                row.querySelectorAll("th, td"),
-                (cell) => cell.textContent
-              )
-          )
-        );
-        console.log("Table : ", table);
-        resolve(table);
-      })
-      .catch((err: Error) => {
-        reject(err);
-      });
+    try {
+        await crawlService
+            .commonCrawl(URL, SELECTOR)
+            .then(async (response: ElementHandle) => {
+                const th = await response.evaluate(() =>
+                    Array.from(
+                        document.querySelectorAll(
+                            "#contents > article > div > div.menu_tb > div.lineTop_tbArea.tbScroll > table > tbody > tr"
+                        ),
+                        (row) =>
+                            Array.from(
+                                row.querySelectorAll("th"),
+                                (cell) => cell.textContent
+                            )
+                    )
+                );
+
+                const td = await response.evaluate(() =>
+                    Array.from(
+                        document.querySelectorAll(
+                            "#contents > article > div > div.menu_tb > div.lineTop_tbArea.tbScroll > table > tbody > tr"
+                        ),
+                        (row) =>
+                            Array.from(
+                                row.querySelectorAll("td"),
+                                (cell) => cell.textContent
+                            )
+                    )
+                );
+
+                let result = new Map<string, any[]>();
+                for(let i=0;i<td.length;i++) {
+                    result.set(th[i].toString(), td[i]);
+                }
+                for(const key of result.keys()) {
+                    console.log(`KEY : ${key}`);
+                    console.log(`result : ${result.get(key)}`);
+                }
+                resolve(result);
+            })
+            .catch((err: Error) => {
+                reject(err);
+            });
+    } catch (e) {
+        reject(e);
+    }
   });
 }
 
