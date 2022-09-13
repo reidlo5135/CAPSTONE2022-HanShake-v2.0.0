0dec64f01f072e7c80771b5eb799c214151734ff
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-13 10:51:00 +0900

    FEAT: Add RestCommunicationException.java

--- server/boot/src/main/java/kr/co/handshake/common/advice/CommonRestControllerAdvice.java
+++ server/boot/src/main/java/kr/co/handshake/common/advice/CommonRestControllerAdvice.java
@@ -2,6 +2,7 @@ package kr.co.handshake.common.advice;
 
 import kr.co.handshake.common.application.ResponseService;
 import kr.co.handshake.common.domain.CommonResult;
+import kr.co.handshake.common.exception.RestCommunicationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
-import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -28,20 +28,23 @@ public class CommonRestControllerAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult defaultException(HttpServletRequest request, Exception e) {
         e.printStackTrace();
-        return responseService.getFailResult
-                (Integer.parseInt(getMessage("unKnown.code")), getMessage("unKnown.msg"));
+        return responseService.getFailResult(
+                Integer.parseInt(getMessage("unKnown.code")), getMessage("unKnown.msg")
+        );
     }
 
     /**
-     * -1004
-     * 권한이 없는 리소스를 요청한 경우 발생 시키는 예외
+     *
+     * -1000
+     * rest Communication Exception
      */
-    @ExceptionHandler(AccessDeniedException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    protected CommonResult accessDeniedException(HttpServletRequest request, AccessDeniedException e) {
+    @ExceptionHandler(RestCommunicationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected CommonResult restCommunicationException(Exception e) {
         e.printStackTrace();
         return responseService.getFailResult(
-                Integer.parseInt(getMessage("accessDenied.code")), getMessage("accessDenied.msg"));
+                Integer.parseInt(getMessage("restCommunicationException.code")), getMessage("restCommunicationException.msg")
+        );
     }
 
     private String getMessage(String code) {
