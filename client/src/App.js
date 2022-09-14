a579a184743c95abeaeaedbfa353f3d98c87682a
dongho <ehdrh321@gmail.com>
2022-09-14 16:24:01 +0900

    09/14 16:23 Add : 각 페이지 Router 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import {BrowserRouter,Route, Switch} from 'react-router-dom';
 import Main from "./page/MainPage/Main";
 import Select from "./page/SelectPage/Select";
+import Food from "./page/FoodPage/Food";
+import Calendar from "./page/CalendarPage/Calendar";
+import Campus from "./page/CampusPage/campus";
 import {AnimatePresence} from "framer-motion";
-
+import CampusDetail from './page/CampusPage/campusDetail';
 
 function App() {
   return (
@@ -14,6 +17,10 @@ function App() {
          <Switch>
              <Route exact path = '/' component={Main}/>
              <Route exact path = '/select' component={Select}/>
+             <Route exact path = '/food' component={Food}/>
+             <Route exact path = '/calendar' component={Calendar}/>
+             <Route exact path = '/campus' component={Campus}/>
+             <Route exact path = '/campusdetail' component={CampusDetail}/>
          </Switch>
         </BrowserRouter>
       </AnimatePresence>
