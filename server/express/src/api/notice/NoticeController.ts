4b753a8545b9a036fa269c0f4ecbff87d9d5d320
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-18 16:41:27 +0900

    Refactor: Modify Notice API Logics

--- server/express/src/api/notice/NoticeController.ts
+++ server/express/src/api/notice/NoticeController.ts
@@ -1,20 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import service from '../../services/notice/scholar/ScholarNoticeService';
+import scholarService from "../../services/notice/scholar/ScholarNoticeService";
+import andService from "../../services/notice/and/AnDNoticeService";
+import bachelorService from "../../services/notice/bachelor/BachelorNoticeService";
+import bidService from "../../services/notice/bid/BidNoticeService";
 
 const {asyncWrapper} = require("../../middlewares/AsyncWrapper");
 
 const findScholarNoticeAll = asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
-    service.crawlScholarNoticeAll().then(
+    scholarService.crawlScholarNoticeAll().then(
         (resolve: Response) => {
             console.log(
-                "NoticeController findNoticeAll promise resolved : ",
+                "NoticeController findScholarNoticeAll promise resolved : ",
                 resolve
             );
             res.status(200).send({ resolve });
         },
         (reject: Error) => {
             console.log(
-                "NoticeController findNoticeAll promise rejected : ",
+                "NoticeController findScholarNoticeAll promise rejected : ",
                 reject
             );
             res.send({ code: -1, message: "failed", error: JSON.stringify(reject) });
@@ -22,6 +25,66 @@ const findScholarNoticeAll = asyncWrapper(async (req: Request, res: Response, ne
     );
 });
 
+const findAnDNoticeAll = asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    andService.crawlAnDNoticeAll().then(
+        (resolve: Response) => {
+            console.log(
+                "NoticeController findAnDNoticeAll promise resolved : ",
+                resolve
+            );
+            res.status(200).send({ resolve });
+        },
+        (reject: Error) => {
+            console.log(
+                "NoticeController findAnDNoticeAll promise rejected : ",
+                reject
+            );
+            res.send({ code: -1, message: "failed", error: JSON.stringify(reject) });
+        }
+    )
+});
+
+const findBachelorNoticeAll = asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    bachelorService.crawlBachelorNoticeAll().then(
+        (resolve: Response) => {
+            console.log(
+                "NoticeController findBachelorNoticeAll promise resolved : ",
+                resolve
+            );
+            res.status(200).send({ resolve });
+        },
+        (reject: Error) => {
+            console.log(
+                "NoticeController findBachelorNoticeAll promise rejected : ",
+                reject
+            );
+            res.send({ code: -1, message: "failed", error: JSON.stringify(reject) });
+        }
+    )
+});
+
+const findBidNoticeAll = asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
+    bidService.crawlBidNoticeAll().then(
+        (resolve: Response) => {
+            console.log(
+                "NoticeController findBiDNoticeAll promise resolved : ",
+                resolve
+            );
+            res.status(200).send({ resolve });
+        },
+        (reject: Error) => {
+            console.log(
+                "NoticeController findBiDNoticeAll promise rejected : ",
+                reject
+            );
+            res.send({ code: -1, message: "failed", error: JSON.stringify(reject) });
+        }
+    )
+});
+
 export = {
-    findScholarNoticeAll
+    findScholarNoticeAll,
+    findAnDNoticeAll,
+    findBachelorNoticeAll,
+    findBidNoticeAll
 }
--- server/express/src/api/notice/NoticeRouter.ts
+++ server/express/src/api/notice/NoticeRouter.ts
@@ -4,5 +4,8 @@ import controller from './NoticeController';
 const router = express.Router();
 
 router.get("/scholar", controller.findScholarNoticeAll);
+router.get("/and", controller.findAnDNoticeAll);
+router.get("/bachelor", controller.findBachelorNoticeAll);
+router.get("/bid", controller.findBidNoticeAll);
 
 export = router;
