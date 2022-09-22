66ad4f6dcd2b6405f07c37820589b1622bcb1db2
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-22 10:29:10 +0900

    Refactor: Modify ScheduleController.java

--- server/boot/src/main/java/kr/co/handshake/schedule/presentation/ScheduleController.java
+++ server/boot/src/main/java/kr/co/handshake/schedule/presentation/ScheduleController.java
@@ -1,5 +1,6 @@
 package kr.co.handshake.schedule.presentation;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import kr.co.handshake.common.domain.ListResult;
 import kr.co.handshake.schedule.application.ScheduleService;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"3. Schedule"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/v1/api/schedule/")
