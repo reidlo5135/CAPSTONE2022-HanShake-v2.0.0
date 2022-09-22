fded86c5b11805766176c56c1235cef7e2d4d343
reidlo5135 <201840103@email.daelim.ac.kr>
2022-09-22 10:25:57 +0900

    Refactor: Modify Schedule Crawling API

--- server/express/src/services/schedule/ScheduleService.ts
+++ server/express/src/services/schedule/ScheduleService.ts
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const {promiseWrapper} = require("../../../middlewares/AsyncWrapper");
+const {promiseWrapper} = require("../../middlewares/AsyncWrapper");
 
 const MENU_ID: any = process.env.SCHEDULE_MENU_ID || 930;
 const SELECTOR: string =
-    "#contents > article > div > div#month3 > ul.calList_con";
+    "#contents > article > div.academicCal > div > ul.calList_con";
 
 let result = new Map<string, any[]>();
 
@@ -31,7 +31,7 @@ async function commonTargetCrawl (response: ElementHandle) {
     const date = await response.evaluate(() =>
         Array.from(
             document.querySelectorAll(
-                "#contents > article > div > div#month3 > ul.calList_con > li"
+                "#contents > article > div > div > ul.calList_con > li"
             ),
             (row) =>
                 Array.from(
@@ -41,8 +41,36 @@ async function commonTargetCrawl (response: ElementHandle) {
         )
     );
 
+    const type = await response.evaluate(() =>
+        Array.from(
+            document.querySelectorAll(
+                "#contents > article > div > div > ul.calList_con > li > p"
+            ),
+            (row) =>
+                Array.from(
+                    row.querySelectorAll("span.sort"),
+                    (cell) => cell.textContent
+                )
+        )
+    );
+
+    const details = await response.evaluate(() =>
+        Array.from(
+            document.querySelectorAll(
+                "#contents > article > div > div > ul.calList_con > li"
+            ),
+            (row) =>
+                Array.from(
+                    row.querySelectorAll("p"),
+                    (cell) => cell.textContent
+                )
+        )
+    );
+
     let map = new Map<string, string>();
     for (let i=0;i<date.length;i++) {
+        map.set("type", type[i].toString());
+        map.set("details", details[i].toString());
         result.set(date[i].toString(), JSON.parse(JSON.stringify(Object.fromEntries(map))));
     }
     return result;
