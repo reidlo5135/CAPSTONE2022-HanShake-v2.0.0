1515d9efce2f14ca104e06ca3f2c46ab32debdeb
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-23 11:36:44 +0900

    Feat: Add Facility Classes

--- server/boot/src/main/java/kr/co/handshake/facility/application/FacilityService.java
+++ server/boot/src/main/java/kr/co/handshake/facility/application/FacilityService.java
@@ -1,4 +1,11 @@
 package kr.co.handshake.facility.application;
 
+import kr.co.handshake.facility.domain.FacilityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class FacilityService {
+    private final FacilityRepository facilityRepository;
 }
