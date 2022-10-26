4f3351b88becdb9d11ace2fd5088698e15717ecb
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-26 15:04:07 +0900

    Refactor: Modify Facility API

--- server/boot/src/main/java/kr/co/handshake/facility/dto/FacilityResponseDto.java
+++ server/boot/src/main/java/kr/co/handshake/facility/dto/FacilityResponseDto.java
@@ -1,4 +1,23 @@
 package kr.co.handshake.facility.dto;
 
+import kr.co.handshake.facility.domain.Facility;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class FacilityResponseDto {
+    private String building;
+    private String department;
+    private String welfare;
+
+    public Facility toEntity() {
+        return Facility.builder()
+                .building(building)
+                .department(department)
+                .welfare(welfare)
+                .build();
+    }
 }
