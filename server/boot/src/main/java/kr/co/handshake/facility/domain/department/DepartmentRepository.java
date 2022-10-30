b57bfb5e772be3b608f614e3018212578526f45f
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-30 13:09:48 +0900

    Feat: Add Department Model

---
+++ server/boot/src/main/java/kr/co/handshake/facility/domain/department/DepartmentRepository.java
@@ -0,0 +1,6 @@
+package kr.co.handshake.facility.domain.department;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
