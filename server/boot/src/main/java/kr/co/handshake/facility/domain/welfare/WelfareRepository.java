39d1b3c9663f888bcf035f10c24be828a000d2b6
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-30 13:09:57 +0900

    Feat: Add Welfare Model

---
+++ server/boot/src/main/java/kr/co/handshake/facility/domain/welfare/WelfareRepository.java
@@ -0,0 +1,6 @@
+package kr.co.handshake.facility.domain.welfare;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WelfareRepository extends JpaRepository<Welfare, Long> {
+}
