f817827bd72606476671e2decef58ea6ba2f9d2c
reidlo5135 <201840103@email.daelim.ac.kr>
2022-10-30 13:09:38 +0900

    Feat: Add Building Model

---
+++ server/boot/src/main/java/kr/co/handshake/facility/domain/building/BuildingRepository.java
@@ -0,0 +1,6 @@
+package kr.co.handshake.facility.domain.building;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+}
